35b1ecdcb932be4f7c9be1d85ad54eaf2e65cd80
Liana Manucharyan <liana.manucharyan002@gmail.com>
2024-09-06 13:50:42 -0300

    Arreglo de login

--- JS/login.js
+++ JS/login.js
@@ -1,5 +1,7 @@
 const loginForm = document.querySelector('#loginForm');
 
+    window.location.href = '../PAGES/login.html';
+
     if (loginForm) {
         loginForm.addEventListener('submit', (e) => {
             e.preventDefault();
